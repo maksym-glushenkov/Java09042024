7c1766cb164ec187c769d2f975d4e94a60fe379a
maksym-glushenkov <160600087+maksym-glushenkov@users.noreply.github.com>
2024-04-14 22:59:17 +0200

    HW09042024

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+int number = 324;
+int fNumber = number / 100;
+int sNumber = (number / 10) % 10;
+int tNumber = number % 10;
+System.out.println(fNumber + "," + sNumber +  "," + tNumber );
+
+    }
+}
